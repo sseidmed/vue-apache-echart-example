2ceaa6ca8c18f148f3a826d14671415510bd9e24
Shay Seidmedova <sseidmed@gmail.com>
2023-01-12 12:29:15 -0500

    Add dummy routes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@ import SplineAreaChart from './components/SplineAreaChart.vue'
 <template>
   <div id="nav">
     <router-link to="/">Spline Area Chart</router-link> |
-    <router-link to="/apache">Apache EChart</router-link>
+    <router-link to="/apache">Basic Area Chart</router-link> |
+    <router-link to="/apache">Donut chart</router-link> |
+    <router-link to="/apache">Pyramid Chart</router-link> |
+    <router-link to="/apache">Stream Graph</router-link> |
+    <router-link to="/apache">Apache EChart</router-link> |
   </div>
   <router-view/>
   <SplineAreaChart />
